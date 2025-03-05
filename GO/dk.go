9eebd9dbbe8ba25312ce379c2dee1936adbcff27
DHINESHKUMAR M <dhineshdeveloper07@gmail.com>
2025-03-05 08:06:31 +0530

    Add files via upload

---
+++ GO/dk.go
@@ -0,0 +1,111 @@
+// package main
+// import (
+// 	"fmt"
+// 	"math"
+// )
+// // go lang is an case - sensitive
+// var a = 9;
+
+
+// func add( num1 int, num2 int)int{
+// 	var out = num1+num2;
+// 	return out ;
+// }
+
+// func sub(n1 int,n2 int)int{
+// 	var out = n1+n2;
+// 	return out
+// }
+
+// func calc(n1 int ,n2 int)(int,int){
+// 	var out1 = n1+n2;
+// 	var out2 = n1-n2;
+// 	return out1,out2
+// }
+
+// func local(){
+// 	var a = 10;
+// 	fmt.Println("local variable",a)
+// }
+
+// func main(){
+
+// 	// var num int = 2
+// 	// var num1 int =3;
+
+// 	// var result = num+num1;
+// 	// var num2 float64 = 35.2;
+
+// 	// var n1,n2 int 
+// 	// n1 = 10;
+// 	// n2= 20;
+// 	// fmt.Println(n1+n2)
+
+// 	// fmt.Println("Hello, world")
+// 	// fmt.Println(2+3)
+// 	// fmt.Println(num)
+// 	// fmt.Println(num1)
+// 	// fmt.Println(result)
+
+// 	// fmt.Println(num2)
+
+// 	var n = 9;
+// 	n= 10;
+// 	fmt.Println(n)
+
+// 	const number  = 20;
+// 	fmt.Println(number);
+
+// 	//loop 
+// 	// syntax 1
+// 	// for {
+// 	// 	fmt.Println("DK");
+// 	// }
+
+// 	// syntax 2
+// 	i:=1
+// 	for i<=5{
+// 		fmt.Println("Dk",i)
+// 		i++
+// 	}
+
+// 	for i:=1;i<=5;i++{
+// 		fmt.Println("DK",i);
+// 	}
+
+// 	// function
+
+// 	num1 := 5;
+// 	num2 := 10;
+// 	 result := add(num1,num2);
+// 	 fmt.Println(result);
+
+// 	 n1 := 3;
+// 	 n2 := 2;
+// 	 res := sub(n1,n2);
+// 	 fmt.Println(res)
+
+// 	 r1,r2 := calc(n1,n1);
+// 	 fmt.Println(r1,r2);
+
+// 	 local()
+// 	 fmt.Println("package level or global variable: ",a);
+
+// 	 // inbuilt functions Math
+// 	 var num float64 = 9;
+// 	 var re = math.Sqrt(num)
+// 	 fmt.Println(re)
+
+
+// 	 d := 1
+// 	 if d < 5{
+// 		fmt.Println("Hi",d)
+// 	 }else{
+// 		fmt.Println("Bye",d)
+// 	 }
+
+// }
+
+// func Demo(){
+
+// }
---
+++ GO/variable.go
@@ -0,0 +1,7 @@
+// package main;
+// import ("fmt")
+
+// func main(){
+// 	var num int = 2;
+// 	fmt.Println(num)
+// }
